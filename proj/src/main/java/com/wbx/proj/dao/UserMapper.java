c64647c97d163379a497caecddafc4211e1f5266
wbx <248216195@qq.com>
2023-02-08 17:50:38 +0800

    login ok!

--- proj/src/main/java/com/wbx/proj/dao/UserMapper.java
+++ proj/src/main/java/com/wbx/proj/dao/UserMapper.java
@@ -6,17 +6,17 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface UserMapper {
 
-    User selectById(int id);
+    public User selectById(int id);
 
-    User selectByName(String username);
+    public User selectByName(String username);
 
-    User selectByEmail(String email);
+    public User selectByEmail(String email);
 
-    int insertUser(User user);
+    public int insertUser(User user);
 
-    int updateStatus(int id, int status);
+    public int updateStatus(int id, int status);
 
-    int updateHeader(int id, String headerUrl);
+    public int updateHeader(int id, String headerUrl);
 
-    int updatePassword(int id, String password);
+    public int updatePassword(int id, String password);
 }
--- proj/src/main/java/com/wbx/proj/service/UserService.java
+++ proj/src/main/java/com/wbx/proj/service/UserService.java
@@ -1,6 +1,8 @@
 package com.wbx.proj.service;
 
+import com.wbx.proj.dao.LoginTicketMapper;
 import com.wbx.proj.dao.UserMapper;
+import com.wbx.proj.entity.LoginTicket;
 import com.wbx.proj.entity.User;
 import com.wbx.proj.util.MailClient;
 import com.wbx.proj.util.ProjConstant;
@@ -28,6 +30,9 @@ public class UserService implements ProjConstant {
 
     @Autowired
     private TemplateEngine templateEngine;
+
+    @Autowired
+    private LoginTicketMapper loginTicketMapper;
 
     @Value("proj.path.domain")
     private String domain;
@@ -73,7 +78,7 @@ public class UserService implements ProjConstant {
 
         // 添加用户
         user.setSalt(ProjUtil.generateUUID().substring(0, 5));
-        user.setPassword(ProjUtil.md5(user.getUsername()) + user.getSalt());
+        user.setPassword(ProjUtil.md5(user.getPassword() + user.getSalt()));
         user.setStatus(0);
         user.setType(0);
         user.setActivationCode(ProjUtil.generateUUID());
@@ -102,6 +107,87 @@ public class UserService implements ProjConstant {
         } else {
             return ACTIVATION_FAILURE;
         }
+    }
+
+    public Map<String,Object> login(String username, String password, int expiredSeconds) {
+        HashMap<String,Object> map = new HashMap<>();
+
+        // 验证为空
+        if (StringUtils.isBlank(username)) {
+            map.put("usernameMsg","用户名为空！");
+            return map;
+        }
+        if (StringUtils.isBlank(password)) {
+            map.put("passwordMsg","密码为空！");
+            return map;
+        }
+
+        // 验证账户
+        User user = userMapper.selectByName(username);
+        if (user == null) {
+            map.put("usernameMsg","用户不存在！");
+            return map;
+        }
+        if (user.getStatus() == 0) {
+            map.put("usernameMsg","用户未激活！");
+            return map;
+        }
+
+        // 验证密码
+        String pssd = ProjUtil.md5(password + user.getSalt());
+        if (!user.getPassword().equals(pssd)) {
+            map.put("passwordMsg","密码错误！");
+            return map;
+        }
+
+        // 注册ticket
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(user.getId());
+        loginTicket.setStatus(0);
+        loginTicket.setTicket(ProjUtil.generateUUID());
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + expiredSeconds * 1000));
+        loginTicketMapper.insertLoginTicket(loginTicket);
+
+        map.put("ticket", loginTicket.getTicket());
+
+        return map;
+    }
+
+    public void logout(String ticket) {
+        loginTicketMapper.updateStatus(ticket, 1);
+    }
+
+    public LoginTicket selectByTicket (String ticket) {
+        return loginTicketMapper.selectByTicket(ticket);
+    }
+
+    public int updateHeader(int id, String headerUrl) {
+        return userMapper.updateHeader(id, headerUrl);
+    }
+
+    public Map<String, Object> updatePassword (int id, String oldPassword, String newPassword, String confirmPassword) {
+        Map<String, Object> map = new HashMap<>();
+        User user = userMapper.selectById(id);
+
+        String s = ProjUtil.md5(oldPassword + user.getSalt());
+        if (!s.equals(user.getPassword())) {
+            map.put("oldPasswordMsg", "旧密码错误！");
+            return map;
+        }
+
+        if (!newPassword.equals(confirmPassword)) {
+            map.put("confirmPasswordMsg", "两次输入的密码不一致");
+            return map;
+        }
+
+        String pssd = ProjUtil.md5(newPassword + user.getSalt());
+        if (oldPassword.equals(pssd)) {
+            map.put("newPasswordMsg", "新密码不能与旧密码相同!");
+            return map;
+        }
+
+        userMapper.updatePassword(id, pssd);
+        return map;
     }
 
 }
